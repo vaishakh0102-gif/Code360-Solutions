15bc464aa2006b79149e792d1b5f0b1d1b7c8427
Vaishakh <vaishakh0102@gmail.com>
2026-08-15 11:49:26 +0530

    Create Leaders in an array.cpp

---
+++ Leaders in an array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+vector<int> findLeaders(vector<int> &elements, int n) {
+    vector<int>ans;
+    int maxi=INT_MIN;
+    
+    for (int i=n-1;i>=0;i--){
+        maxi=max(maxi,elements[i]);
+        if(ans.size()==0|| ans.back()!=maxi){
+            ans.push_back(elements[i]);
+        }
+
+
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+    // Write your code here.
+}
